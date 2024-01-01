2c8af7d6ba4e5df6f19e60a4d5d77d0a5917b5da
Abbosjon Keldiyarov <abbosjon99@gmail.com>
2024-01-01 17:47:16 +0100

    updated for vercel

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qu83kf-9d2*hku(@i8cm=^#*nk_1f4y#7^dddl!mtt1250-op@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
